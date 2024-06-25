6c0686b0aac9c9df9b6124a8b41c1ffb6a45464d
enoch <lanxenet@gmail.com>
2024-06-25 15:24:59 +0800

    docs: 添加 Server 的注释

--- app/server/server.go
+++ app/server/server.go
@@ -8,11 +8,11 @@ package server
 
 import "context"
 
-// 服务接口，又 #app 统一管理
+// Server 服务接口，又 #app 统一管理
 type Server interface {
 	// Start  启动服务
-	Start(ctx context.Context) error
+	Start(context.Context) error
 
 	// Stop 停止服务
-	Stop(ctx context.Context) error
+	Stop(context.Context) error
 }
